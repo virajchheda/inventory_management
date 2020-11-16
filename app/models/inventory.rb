7433c6841ea7f0de25612afc4648c571ab025f0f
virajchheda <viraj.chheda@mirraw.com>
2020-11-17 00:10:56 +0530

    product index page query chnages

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -5,4 +5,6 @@ class Inventory < ApplicationRecord
   # validates_presence_of :item_count, :low_item_threshold
   validates :item_count, numericality: { greater_than: -1, message: "cannot be in negative" }
   validates :low_item_threshold, numericality: { greater_than: -1, message: "cannot be in negative"}
+
+  scope :order_by_warehouse, -> { order(warehouse_id: :asc) }
 end
